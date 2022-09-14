d2eaf223e60bdae07fb266f5430d7c54f9dfd762
Johannes Demissie <demissie.johannes@gmail.com>
2022-09-14 18:52:32 +0200

    Test delete functionality.

--- app.js
+++ app.js
@@ -3,43 +3,41 @@ const port = 3000;
 const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get("/", (req, res) => {
-  let cafes = [];
-  // open the database and make a query
-  let db = new sqlite3.Database("./cafes.db");
-  let sql = `SELECT * FROM cafe`;
+app
+  .get("/", (req, res) => {
+    // List all cafe
 
-  db.each(sql, (err, row) => {
-    if (err) {
-      cafes.push(err);
-    } else {
-      cafes.push({ id: row.id, name: row.name, address: row.map_url });
-    }
-  });
-  db.close(() => {
-    res.json({ ...cafes });
-  });
-});
+    let cafes = [];
+    // open the database and make a query
+    let db = new sqlite3.Database("./cafes.db");
+    let sql = `SELECT * FROM cafe`;
 
-app.get("/some", (req, res) => {
-  let cafes = [];
-  // open the database and make a query
-  let db = new sqlite3.Database("./cafes.db");
-  let sql = `SELECT * FROM cafe WHERE id < 10 AND id > 7`;
+    db.each(sql, (err, row) => {
+      if (err) {
+        cafes.push(err);
+      } else {
+        cafes.push({ id: row.id, name: row.name, address: row.map_url });
+      }
+    });
+    db.close(() => {
+      res.json({ ...cafes });
+    });
+  })
+  .delete("/", (req, res) => {
+    // Delete chosen cafe
+    
+    var id = req.body.id;
+    let db = new sqlite3.Database("./cafes.db");
+    let sql = `DELETE FROM cafe WHERE id=${id}`;
+    db.run(sql);
+    db.close();
+    console.log(`Delete id: ${id}`);
+    res.redirect("/");
+  });
 
-  db.each(sql, (err, row) => {
-    if (err) {
-      cafes.push(err);
-    } else {
-      cafes.push({ id: row.id, name: row.name, address: row.map_url });
-    }
-  });
-  db.close(() => {
-    res.json({ ...cafes });
-  });
-});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app run on http://localhost:${port}/`);
 });
